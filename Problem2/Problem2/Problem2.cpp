a973280126adc2107eddd8837aa659d69d2971ef
Jack Sheppard <jsheppard@Jacks-MBP.connect>
2017-10-10 21:57:06 +0100

    Completed Problem2

---
+++ Problem2/Problem2/Problem2.cpp
@@ -0,0 +1,9 @@
+//
+//  Problem2.cpp
+//  Problem2
+//
+//  Created by Jack Sheppard on 08/10/2017.
+//  Copyright © 2017 Jack Sheppard. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ Problem2/Problem2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Problem2
+//
+//  Created by Jack Sheppard on 08/10/2017.
+//  Copyright © 2017 Jack Sheppard. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
